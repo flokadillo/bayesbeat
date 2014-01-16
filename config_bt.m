f354a59664f375c327788ab3a992552b77ec87cf
Florian Krebs <florian.krebs@jku.at>
2014-01-16 15:59:23 +0100

    robo start up

--- config_bt.m
+++ config_bt.m
@@ -29,7 +29,7 @@ Params.useTempoPrior = 0;
 Params.patternGiven = 0;
 Params.doLeaveOneOut = 0;
 Params.save_inference_data = 0;
-Params.inferenceMethod = 'PF'; % 'HMM_viterbi', 'HMM_forward', 'PF'
+Params.inferenceMethod = 'HMM_forward'; % 'HMM_viterbi', 'HMM_forward', 'PF'
 % Params.trainObservationModel = 1;
 % Params.trainTransitionMatrix = 1;
 
@@ -38,15 +38,15 @@ Params.inferenceMethod = 'PF'; % 'HMM_viterbi', 'HMM_forward', 'PF'
 
 % Params.M = 2560/1440; % number of discrete position states
 % Params.N = 47/26;
-Params.M = 1440; % total number of discrete position states (used for the meter with the longest duration)
-Params.N = 26;
-Params.R = 2;
-Params.meters = [3, 4; 4, 4]; % e.g., [9, 3; 8 4]
+Params.M = 480; % total number of discrete position states (used for the meter with the longest duration)
+Params.N = 35;
+Params.R = 1;
+Params.meters = [1; 4]; % e.g., [9, 3; 8 4]
 Params.T = size(Params.meters, 2);
 bar_durations = Params.meters(1, :) ./ Params.meters(2, :);
 meter2M = Params.M ./ max(bar_durations);
 Params.Meff = round(bar_durations * meter2M);
-Params.pattern_size = 'bar'; % 'beat' or 'bar'
+Params.pattern_size = 'beat'; % 'beat' or 'bar'
 Params.pn = 0.001;  % 7 for n dependent p_n
 Params.pr = 0;
 % Params.pr = 1 - 1/Params.R; % probability of change of rhythmic pattern
@@ -78,13 +78,13 @@ end
 % Observation feature
 Params.observationModelType = 'MOG';  % types = {invGauss, fixed, gamma, histogram, multivariateHistogram,
 % bivariateGauss, ... mixOfGauss, MOG, MOG3}
-Params.feat_type{1} = 'lo230_superflux.mvavg.normZ';
-Params.feat_type{2} = 'hi250_superflux.mvavg.normZ';
+% Params.feat_type{1} = 'lo230_superflux.mvavg.normZ';
+% Params.feat_type{2} = 'hi250_superflux.mvavg.normZ';
 %      Params.feat_type{1} = 'bt.SF.filtered82.log';
 %      Params.feat_type{2} = 'mid250_425_superflux.mvavg.normZ';
 %      Params.feat_type{3} = 'hi450_superflux.mvavg.normZ';
 % Params.feat_type{1} = 'superflux.mvavg.normZ';
-% Params.feat_type{1} = 'sprflx-online';
+Params.feat_type{1} = 'sprflx-online';
 %      Params.feat_type{1} = 'bt.SF.filtered82.log';
 Params.featureDim = length(Params.feat_type);
 % make filename where features are stored
@@ -112,7 +112,7 @@ if ~Params.doTraining
 end
 
 % % test data
-Params.test_set = 'boeck-sisr-problems';
+Params.test_set = 'robo_test';
 Params.testLab = ['~/diss/data/beats/', Params.test_set, '.lab'];
 % Params.test_set = ' ';
 % Params.testLab = '~/diss/data/beats/boeck/train13.wav';
